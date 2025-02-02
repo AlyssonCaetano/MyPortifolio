2a909cd55c9b99615cc7c8affb35b7a51279ee7b
Caetano <122699973+AlyssonCaetano@users.noreply.github.com>
2025-02-02 12:44:59 -0300

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       olá
+<<<<<<< HEAD
+    </div>
+    )
+=======
     <div>  
   )
+>>>>>>> ae51bfe (teste)
 }
 
 export default App
